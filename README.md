a0e8dacb271fd6967fad919ec2777c6c0d37ac6e
imadelayoubi <imadelayoubi07@gmail.com>
2021-10-19 13:34:55 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-vqy2hj-tududb
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-vqy2hj-tududb)
---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+import { products, Product } from '../products';
+
+@Component({
+  selector: 'app-product-list',
+
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css'],
+})
+export class ProductListComponent {
+  products: Product[] = products;
+
+  share() {
+    window.alert('The product has been shared!');
+  }
+
+  play() {
+    console.log('hello');
+  }
+}
